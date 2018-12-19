5a25ea9522aebe37d1fc2c0d5f1b69242fe0ccb4
getmubarak <getmub@gmail.com>
2018-12-19 11:47:06 +0530

    Update transfer.java

--- transfer.java
+++ transfer.java
@@ -1,30 +1,30 @@
 public class UglyMoneyTransferService 
 {
-public void transferFunds(Account source, Account target, BigDecimal amount, boolean allowDuplicateTxn){
-Account sourceAccount = null;
-if(rs.next()) {
-sourceAccount = new Account();
-//populate account
-}
-Account targetAccount = null;
-if(!sourceAccount.isOverdraftAllowed()) {
-if((sourceAccount.getBalance() - amount) < 0) {
-throw new RuntimeException("Insufficient Balance");
-}
-}
-else {
-if(((sourceAccount.getBalance()+sourceAccount.getOverdraftLimit()) - amount) < 0) {
-throw new RuntimeException("Insufficient Balance, Exceeding Overdraft Limit");
-}
-}
-AccountTransaction lastTxn = .. ; //JDBC code to obtain last transaction of sourceAccount
-if(lastTxn != null) {
-if(lastTxn.getTargetAcno().equals(targetAccount.getAcno()) && lastTxn.getAmount() == amount && !allowDuplicateTxn) {
-throw new RuntimeException("Duplicate transaction exception");
-}
-}
-sourceAccount.debit(amount);
-targetAccount.credit(amount);
-}
+  public void transferFunds(Account source, Account target, BigDecimal amount, boolean allowDuplicateTxn){
+    Account sourceAccount = null;
+    if(rs.next()) {
+      sourceAccount = new Account();
+      //populate account
+    }
+    Account targetAccount = null;
+    if(!sourceAccount.isOverdraftAllowed()) {
+      if((sourceAccount.getBalance() - amount) < 0) {
+        throw new RuntimeException("Insufficient Balance");
+      }
+    }
+    else {
+      if(((sourceAccount.getBalance()+sourceAccount.getOverdraftLimit()) - amount) < 0) {
+        throw new RuntimeException("Insufficient Balance, Exceeding Overdraft Limit");
+      }
+    }
+    AccountTransaction lastTxn = .. ; //JDBC code to obtain last transaction of sourceAccount
+    if(lastTxn != null) {
+      if(lastTxn.getTargetAcno().equals(targetAccount.getAcno()) && lastTxn.getAmount() == amount && !allowDuplicateTxn) {
+        throw new RuntimeException("Duplicate transaction exception");
+      }
+    }
+    sourceAccount.debit(amount);
+    targetAccount.credit(amount);
+  }
 
 }
