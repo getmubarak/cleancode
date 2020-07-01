4ab87f85e1db12d3444275c32f4237339bddc0cb
getmubarak <getmub@gmail.com>
2020-07-01 11:38:39 +0530

    Update Pay.java

--- Pay.java
+++ Pay.java
@@ -2,10 +2,8 @@ double getPayAmount() {
   double result;
   if (_isDead) result = deadAmount();
   else {
-   //logic
     if (_isSeparated) result = separatedAmount();
     else {
-      //logic
       if (_isRetired) result = retiredAmount();
       else result = normalPayAmount();
     };
